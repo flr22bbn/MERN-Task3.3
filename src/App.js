6194812919e2ce21269926808b5419f4a90549ce
flr22bbn <floraboban22@gmail.com>
2023-10-15 22:18:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState } from "react";
 
 function App() {
+  const [currColor, setCurrColor] = useState("lightpink");
+
+  const [display, setDisplay] = useState("lightpink");
+
+  const updateColor = (col) => {
+    setCurrColor(col)
+    setDisplay(!display)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Picker</h1>
+      <div className='container' style={{ backgroundColor: currColor }}>
+        {display && (
+          <div>
+            <button
+              style={{ backgroundColor: "red" }}
+              onClick={() => updateColor("red")}
+            ></button>
+            <button
+              style={{ backgroundColor: "Chartreuse" }}
+              onClick={() => updateColor("Chartreuse")}
+            ></button>
+            <button
+              style={{ backgroundColor: "blue" }}
+              onClick={() => updateColor("blue")}
+            ></button>
+
+            <button
+              style={{ backgroundColor: "purple" }}
+              onClick={() => updateColor("purple")}
+            ></button>
+            <button
+              style={{ backgroundColor: "lightpink" }}
+              onClick={() => updateColor("lightpink")}
+            ></button>
+            <button
+              style={{ backgroundColor: "DarkOrange" }}
+              onClick={() => updateColor("DarkOrange")}
+            ></button>
+            <button
+              style={{ backgroundColor: "CornflowerBlue" }}
+              onClick={() => updateColor("CornflowerBlue")}
+            ></button>
+            <button
+              style={{ backgroundColor: "gold" }}
+              onClick={() => updateColor("gold")}
+            ></button>
+          </div>
+        )}
+        <div>
+          <button
+            style={{ width: 200 }}
+            onClick={() => {
+              setDisplay(!display);
+            }}
+          >
+            Pick a color
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
